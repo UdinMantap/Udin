c88b271c6cdd6eb47cbdf30ef6d8e53ce7b05106
UdinMantap <145738314+UdinMantap@users.noreply.github.com>
2023-09-22 10:59:53 +0700

    Add files via upload

---
+++ dart latihan.dart
@@ -0,0 +1,9 @@
+void main() {
+// Masukkan panjang alas dan tinggi segitiga
+double alas = 12.0;
+double tinggi = 6.0;
+// Hitung luas segitiga
+double luas = 0.4 * alas * tinggi;
+// Cetak hasil
+print('Luas segitiga adalah: $luas');
+}
